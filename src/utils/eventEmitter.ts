c415cd103ec1ecac31b9824a289c00546161a251
qtinp <lin_ink@outlook.com>
2022-12-02 02:04:58 +0800

    fix:修改个别方法,添加注释

--- src/utils/eventEmitter.ts
+++ src/utils/eventEmitter.ts
@@ -18,8 +18,17 @@ type EventParams<
 
 type ListenerFn<Params extends any[] = any> = (...args: Params) => void;
 
-class Listener<Fn extends ListenerFn = ListenerFn> {
-  constructor(public fn: Fn, public once = false, public context = undefined) {}
+/**
+ * Representation of a single event listener.
+ *
+ * @param {Function} fn The listener function.
+ * @param {Boolean} [once=false] Specify if the listener is a one-time listener.
+ * @param {*} context The context to invoke the listener with.
+ * @constructor
+ * @private
+ */
+class Listener<Fn extends ListenerFn = ListenerFn, Context = undefined> {
+  constructor(public fn: Fn, public once = false, public context?: Context) {}
 }
 
 export type DefineEvents<Map extends DefaultEventMap> = {
@@ -34,7 +43,15 @@ export class EventEmitter<
   constructor() {
     this._events = new Map();
   }
-
+  /**
+   * Add a listener for a given event.
+   *
+   * @param {(String|Symbol|Number)} event The event name.
+   * @param {Function} fn The listener function.
+   * @param {Boolean} once Specify if the listener is a one-time listener.
+   * @returns {EventEmitter<EmitEvents & Record<Ev, Fn>>}
+   * @private
+   */
   addListener<
     Ev extends EventNames<EmitEvents>,
     Fn extends ListenerFn = ListenerFn<EventParams<EmitEvents, Ev>>,
@@ -52,11 +69,13 @@ export class EventEmitter<
     this._events.set(event, listeners);
     return this;
   }
-
-  clearEvent<Ev extends EventNames<EmitEvents>>(event: Ev): boolean {
-    return this._events.delete(event);
-  }
-
+  /**
+   * Return an array listing the events for which the emitter has registered
+   * listeners.
+   *
+   * @returns {Array}
+   * @public
+   */
   eventNames<Ev extends EventNames<EmitEvents>>(): Ev[] {
     const names: any[] = [];
     for (const name of this._events.keys()) {
@@ -64,23 +83,43 @@ export class EventEmitter<
     }
     return names;
   }
-
+  /**
+   * Return the listeners registered for a given event.
+   *
+   * @param {(String|Symbol|Number)} event The event name.
+   * @returns {Array} The registered listeners.
+   * @public
+   */
   listeners<Ev extends EventNames<EmitEvents>>(event: Ev): ListenerFn<any>[] {
     const listeners = this._events.get(event);
     if (!listeners || listeners.size === 0) return [];
     return Array.from(listeners).map((ee) => ee.fn);
   }
-
+  /**
+   * Return the number of listeners listening to a given event.
+   *
+   * @param {(String|Symbol|Number)} event The event name.
+   * @returns {Number} The number of listeners.
+   * @public
+   */
   listenerCount<Ev extends EventNames<EmitEvents>>(event: Ev): number {
     return this._events.get(event)?.size ?? 0;
   }
-
+  /**
+   * Calls each of the listeners registered for a given event.
+   *
+   * @param {(String|Symbol|Number)} event The event name.
+   * @returns {Boolean} `true` if the event had listeners, else `false`.
+   * @public
+   */
   emit<
     Ev extends EventNames<EmitEvents>,
     Params extends EventParams<EmitEvents, Ev>,
   >(event: Ev, ...args: Params): boolean {
     const listeners = this._events.get(event);
-    if (!listeners || listeners.size === 0) return false;
+    if (!listeners || listeners.size === 0) {
+      return false;
+    }
     for (const ee of Array.from(listeners)) {
       ee.fn(...args);
       if (ee.once) {
@@ -94,46 +133,77 @@ export class EventEmitter<
     }
     return true;
   }
-
+  /**
+   * Add a listener for a given event.
+   *
+   * @param {(String|Symbol|Number)} event The event name.
+   * @param {Function} fn The listener function.
+   * @returns {EventEmitter<EmitEvents & Record<Ev, Fn>>} `this`.
+   * @public
+   */
   on<
     Ev extends EventNames<EmitEvents>,
     Fn extends ListenerFn = ListenerFn<EventParams<EmitEvents, Ev>>,
   >(event: Ev, fn: Fn): EventEmitter<EmitEvents & Record<Ev, Fn>> {
     return this.addListener(event, fn, false);
   }
-
+  /**
+   * Add a one-time listener for a given event.
+   *
+   * @param {(String|Symbol|Number)} event The event name.
+   * @param {Function} fn The listener function.
+   * @returns {EventEmitter<EmitEvents & Record<Ev, Fn>>} `this`.
+   * @public
+   */
   once<
     Ev extends EventNames<EmitEvents>,
     Fn extends ListenerFn = ListenerFn<EventParams<EmitEvents, Ev>>,
   >(event: Ev, fn: Fn): EventEmitter<EmitEvents & Record<Ev, Fn>> {
     return this.addListener(event, fn, true);
   }
-
-  off<Ev extends keyof EmitEvents = EventNames<EmitEvents>>(
-    event: Ev,
-  ): EventEmitter<SetDefault<EmitEvents, Ev>> {
-    this.clearEvent(event);
-    return this as EventEmitter<SetDefault<EmitEvents, Ev>>;
-  }
-
+  /**
+   * Alias methods names because people roll like that.
+   */
+  off = this.reomveListener;
+  /**
+   * Alias methods names because people roll like that.
+   */
+  offAny = this.removeAllListeners;
+  /**
+   * Remove the listeners of a given event.
+   *
+   * @param {(String|Symbol|Number)} event The event name.
+   * @param {Function} fn Only remove the listeners that match this function.
+   * @param {Boolean} once Only remove one-time listeners.
+   * @returns {EventEmitter} `this`.
+   * @public
+   */
   reomveListener<
     Ev extends EventNames<EmitEvents>,
     Fn extends ListenerFn<EventParams<EmitEvents, Ev>>,
-  >(event: Ev, fn: Fn): this {
+  >(event: Ev, fn: Fn, once?: boolean): this {
     const listeners = this._events.get(event);
     if (!listeners) {
       return this;
     }
     for (const ee of listeners.values()) {
-      if (ee.fn === fn) {
+      if (ee.fn === fn && (!once || ee.once)) {
         listeners.delete(ee);
       }
     }
     return this;
   }
-
-  removeAllListeners(): boolean {
-    this._events = new Map();
-    return true;
+  /**
+   * Remove all listeners, or those of the specified event.
+   *
+   * @param {(String|Symbol|Number)} [event] The event name.
+   * @returns {EventEmitter<EmitEvents & Record<Ev, Fn>>} `this`.
+   * @public
+   */
+  removeAllListeners<Ev extends EventNames<EmitEvents>>(
+    event: Ev,
+  ): EventEmitter<SetDefault<EmitEvents, Ev>> {
+    this._events.delete(event);
+    return this as EventEmitter<SetDefault<EmitEvents, Ev>>;
   }
 }
